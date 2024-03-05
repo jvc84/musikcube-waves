822d9c2920fff145e8077038122ba3b4d5f4fd21
jvc84 <72560784+jvc84@users.noreply.github.com>
2024-03-05 08:17:13 +0000

    Changed cava termination rules

--- scripts/play_cava.sh
+++ scripts/play_cava.sh
@@ -50,3 +50,4 @@ fi
 
 cava -p "$local_config_file" | sed -u "s/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;" | sed -u "$cut_cava" &     # add dots befor '$' or after '^' to remove bars
 check_state
+
